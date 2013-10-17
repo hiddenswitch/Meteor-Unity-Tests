e2a10c367723416ac6ef1f77c891175479e97407
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2013-10-17 12:31:48 -0700

    Pems and services

---
+++ Meteor/server/lib/services.js
@@ -0,0 +1,30 @@
+/**
+ * @author Benjamin Berman
+ * © 2012 All Rights Reserved
+ **/
+SdkServices = SdkServices || {};
+
+SdkServices.configure = function (options) {
+
+};
+
+// Login with Facebook token
+SdkServices.getMeteorTokenFromFacebookToken = function (fbId, name, email, accessToken, options) {
+    var serviceData = {
+        id: fbId,
+        accessToken: accessToken,
+        email: email
+    };
+
+    var options = _.extend({
+        profile: {
+            name: name
+        }
+    }, options);
+
+    return Accounts.updateOrCreateUserFromExternalService('facebook', serviceData, options);
+};
+
+Meteor.methods({
+    getMeteorTokenFromFacebookToken: SdkServices.getMeteorTokenFromFacebookToken
+});
